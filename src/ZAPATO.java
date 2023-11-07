1fef00c76699f30f1eacd65f73cef6c2a204ca34
valtbarrios2515 <110685743+valtbarrios2515@users.noreply.github.com>
2023-11-07 16:20:10 -0500

    cree ATRIBUTO Y METODO

---
+++ src/ZAPATO.java
@@ -0,0 +1,94 @@
+public class ZAPATO {
+
+    //ATRIBUTOS
+    String Nombre;
+    String Material;
+    String Forma;
+    String Talla;
+    String Color;
+
+    
+    //METODO
+    public static void main(String [] args) {
+    ZAPATO ZAPATO1 = new ZAPATO();
+
+     ZAPATO1.Nombre = " CHANEEL ";
+     ZAPATO1.Material = " TELA ";
+     ZAPATO1.Forma = " DEPORTIVO ";
+     ZAPATO1.Talla = " 30-35 ";
+     ZAPATO1.Color = " AZUL ";
+
+     System.out.println(" EL NOMBRE DE EL ZAPATO ES: "+ZAPATO1.Nombre);
+     System.out.println(" EL MATERIAL DE EL ZAPATO ES: "+ZAPATO1.Material);
+     System.out.println(" LA FORMA DE EL ZAPATO ES: "+ZAPATO1.Forma);
+     System.out.println(" LA TALLA DE EL ZAPATO ES: "+ZAPATO1.Talla);
+     System.out.println(" EL COLOR DE EL ZAPATO ES: "+ZAPATO1.Color);
+
+     ZAPATO ZAPATO2 = new ZAPATO();
+
+        ZAPATO2.Nombre = " CONVERS ";
+        ZAPATO2.Material = " TELA ";
+        ZAPATO2.Forma = " DEPORTIVO ";
+        ZAPATO2.Talla = " 40-45 ";
+        ZAPATO2.Color = " NEGRO ";
+
+        System.out.println(" EL NOMBRE DE EL ZAPATO ES: "+ZAPATO2.Nombre);
+        System.out.println(" EL MATERIAL DE EL ZAPATO ES: "+ZAPATO2.Material);
+        System.out.println(" LA FORMA DE EL ZAPATO ES: "+ZAPATO2.Forma);
+        System.out.println(" LA TALLA DE EL ZAPATO ES: "+ZAPATO2.Talla);
+        System.out.println(" EL COLOR DE EL ZAPATO ES: "+ZAPATO2.Color);
+
+        ZAPATO ZAPATO3 = new ZAPATO();
+
+        ZAPATO3.Nombre = " ADIDAS ";
+        ZAPATO3.Material = " TELA ";
+        ZAPATO3.Forma = " DEPORTIVO ";
+        ZAPATO3.Talla = " 20-25";
+        ZAPATO3.Color = " BLANCAS ";
+
+        System.out.println(" EL NOMBRE DE EL ZAPATO ES: "+ZAPATO3.Nombre);
+        System.out.println(" EL MATERIAL DE EL ZAPATO ES: "+ZAPATO3.Material);
+        System.out.println(" LA FORMA DE EL ZAPATO ES: "+ZAPATO3.Forma);
+        System.out.println(" LA TALLA DE EL ZAPATO ES: "+ZAPATO3.Talla);
+        System.out.println(" EL COLOR DE EL ZAPATO ES: "+ZAPATO3.Color);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
